f42b0501874935cf4cda24a1c62ee77e8bb9d19e
eltoro0815 <th_ru@web.de>
2019-05-26 17:12:26 +0200

    fixed css

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,7 +9,7 @@
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
 
-        <link href="{{ asset('css/app.css') }}" rel="stylesheet" type="text/css" >
+        <link href="css/app.css" rel="stylesheet" type="text/css" >
 
         <!-- Styles -->
         <style>
