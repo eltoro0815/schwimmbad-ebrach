0a470c55b44ed0be07222a8f25df8f90039783c4
eltoro0815 <th_ru@web.de>
2019-05-27 23:30:44 +0200

    production build

---
+++ public/precache-manifest.cbb5017a27751e0273849deb92c2384f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "dc1b66e29e689d52b1ce",
+    "url": "/js/chunk-vendors.7fbfb39a.js"
+  },
+  {
+    "revision": "71e322ba013e679d9027",
+    "url": "/js/app.be752f75.js"
+  },
+  {
+    "revision": "12f685e56ebc821dc97d797a36537fc1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "71e322ba013e679d9027",
+    "url": "/css/app.e62e89db.css"
+  }
+];
