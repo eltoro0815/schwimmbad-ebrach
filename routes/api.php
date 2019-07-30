22ad345ac04dc2e1619c254436426acf8819d2a5
eltoro0815 <th_ru@web.de>
2019-07-30 22:24:21 +0200

    update

--- routes/api.php
+++ routes/api.php
@@ -32,7 +32,7 @@ Route::get('/isopen', function (Request $request) {
     );
 });
 
-Route::get('/toggle', function (Request $request) {
+Route::put('/toggle', function (Request $request) {
     $isopen = Geeoffnet::firstOrCreate(
         ['id' => 1]
     );
