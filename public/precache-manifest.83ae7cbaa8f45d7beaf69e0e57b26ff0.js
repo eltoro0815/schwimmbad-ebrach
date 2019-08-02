54805b544985763647336ac3e086d2f889d97e19
eltoro0815 <th_ru@web.de>
2019-08-02 23:55:54 +0200

    yarn build

---
+++ public/precache-manifest.83ae7cbaa8f45d7beaf69e0e57b26ff0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "98a3f80d09ebbef165dd",
+    "url": "/js/chunk-vendors.6c313cc2.js"
+  },
+  {
+    "revision": "5189774a80205b91fa9b",
+    "url": "/js/app.3ff7eef5.js"
+  },
+  {
+    "revision": "c0249e42d4ed2302dcba686919380e24",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c9835dc97c8f2dc2731da64d6795765e",
+    "url": "/img/autumn-forest.png"
+  },
+  {
+    "revision": "98a3f80d09ebbef165dd",
+    "url": "/css/chunk-vendors.a7ea6f03.css"
+  },
+  {
+    "revision": "5189774a80205b91fa9b",
+    "url": "/css/app.599cd2ba.css"
+  }
+];
--- public/service-worker.js
+++ public/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.e3cfecb06580406003680ff8f434c453.js"
+  "/precache-manifest.83ae7cbaa8f45d7beaf69e0e57b26ff0.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "frontend"});
